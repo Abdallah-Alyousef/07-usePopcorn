015f637bae1cd8341d3103cc4c42a110f51b7f77
abdalluh alyousef <alyousefcs@gmail.com>
2023-09-27 15:42:51 +0300

    ye

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import "./index.css";
 // import App from "./App";
 import StarRating from "./StarRating";
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
 
+  return (
+    <div>
+      <StarRating color="blue" onSetRating={setMovieRating} />;
+      <p>This movie was rated {movieRating} </p>
+    </div>
+  );
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StarRating
+      size={24}
+      color="red"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={2}
+    />
+    <Test />
   </React.StrictMode>
 );
